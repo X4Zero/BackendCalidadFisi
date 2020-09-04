19ffcf3471b5b7683bfcb56f0f9190fe4f4ecc9b
Antony Romero <alexander.romero@unmsm.edu.pe>
2020-09-04 17:45:17 -0500

    Agregando la opción obtener cantidad de encuestados

--- app.py
+++ app.py
@@ -121,6 +121,34 @@ def traer_data_bd():
         }
     return response
 
+@app.route('/alumnos')
+def encuestados():
+    print("TRAER_RESPUESTA_POR_DIMENSION")
+
+    try:
+        cur = mysql.connection.cursor()
+        script_select = "SELECT COUNT(*) FROM alumno;"
+        print("script_select: ",script_select)
+        cur.execute(script_select)
+        respuesta =  cur.fetchone()
+        cantidad_encuestados = respuesta[0]
+        #parseamos las respuestas
+
+        print("cantidad_encuestados: ",cantidad_encuestados)
+
+        response = {
+            "respuesta": {"cantidad encuestados":cantidad_encuestados},
+            "status":200
+        }
+
+    except Exception as e:
+        print(e)
+        response={
+            "respuesta":"Error",
+            "status":500
+        }
+    return response
+
 @app.route('/dimension')
 def respuestas_dimensiones():
     print("TRAER_RESPUESTA_POR_DIMENSION")
@@ -156,9 +184,6 @@ GROUP BY d.nombre, r.respuesta;
             "status":500
         }
     return response
-        
-
-
 # FIN RESPUESTAS - ALUMNOS
 
 # PREGUNTAS
